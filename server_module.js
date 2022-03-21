980701db5605e43092848364e447b7ecfcdc832e
BlancOfficial <87202988+BlancOfficial@users.noreply.github.com>
2022-03-21 21:53:32 +0000

    Hotfix

--- server_module.js
+++ server_module.js
@@ -1,4 +1,4 @@
 const si = require('systeminformation');
 
-server_stat = ["Currently using " + si.mem(free) / si.mem(total) * 100 + "% RAM"]
+server_stat = ["Currently using " + si.mem(free).then(data => console.log(data)) / si.mem(total).then(data => console.log(data)) * 100 + "% RAM"]
 module.exports = {server_stat};
