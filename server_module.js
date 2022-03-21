682a3377e1a592f887aff89a36b4cc403b236d4b
BlancOfficial <87202988+BlancOfficial@users.noreply.github.com>
2022-03-21 22:00:44 +0000

    Removing server_module.js features

--- server_module.js
+++
@@ -1,4 +0,0 @@
-const si = require('systeminformation');
-
-server_stat = ["Currently using " + (si.mem().then(data => console.log(data))).free / (si.mem().then(data => console.log(data))).total * 100 + "% RAM"]
-module.exports = {server_stat};
