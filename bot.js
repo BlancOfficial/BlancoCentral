392e7ba1bf07874cd96d73b8b134b9eb498cf722
BlancOfficial <87202988+BlancOfficial@users.noreply.github.com>
2022-03-24 18:04:57 +0000

    v1.0.9
    A complete Restructure of variable load in

--- bot.js
+++ bot.js
@@ -13,16 +13,6 @@ function interaction_buttons(module_var){
     }
 
 
-const {cat_pictures} = require('./module_store/cat_module.js');
-const {fox_pictures} = require('./module_store/fox_module.js');
-const {ferret_pictures} = require('./module_store/ferret_module.js');
-const {gura_pictures} = require('./module_store/gura_module.js');
-const {koala_pictures} = require('./module_store/koala_module.js');
-const {lion_pictures} = require('./module_store/lion_module.js');
-const {panda_pictures} = require('./module_store/panda_module.js');
-const {wolf_pictures} = require('./module_store/wolf_module.js');
-const {rabbit_pictures} = require('./module_store/rabbit_module.js');
-const {fish_pictures} = require('./module_store/fish_module.js');
 
 const {MessageActionRow} = require('discord.js');
 const {MessageButton} = require('discord.js');
@@ -30,7 +20,7 @@ const Discord = require('discord.js');
 const client = new Discord.Client({ intents: ["GUILDS", "GUILD_MESSAGES"] });
 require('dotenv').config();
 
-const update_info = "https://cdn.discordapp.com/attachments/955121751094882336/956226641514872902/Info_v1.0.8.png"
+var update_info = "https://cdn.discordapp.com/attachments/955121751094882336/956226641514872902/Info_v1.0.8.png"
 
 
 function inter_reply(module_var, output_list){
@@ -83,16 +73,18 @@ client.on('ready', () => {
 client.login(process.env.DISCORD_TOKEN);
 
 bot_reply("a", ["https://tenor.com/view/gawr-gura-gawr-gura-gif-18439720"]);
-bot_reply("info", ["Currently running v1.0.8 of BlancoBot\n\n***UPDATE LOG***\n```Major Image Bank Update\nYou Can now repeat a command by clicking the green button...\nButton times out after 10 seconds :P\nThe Repeated commands are all hidden and only you can see them```\nhttps://cdn.discordapp.com/attachments/955121751094882336/955525144963596348/Thanks.png\n" + update_info]);
+bot_reply("info", ["Currently running v1.0.9 of BlancoBot\n\n***UPDATE LOG***\n```Major Image Bank Update\nYou Can now repeat a command by clicking the green button...\nButton times out after 10 seconds :P\nThe Repeated commands are all hidden and only you can see them```\nhttps://cdn.discordapp.com/attachments/955121751094882336/955525144963596348/Thanks.png\n" + update_info]);
 bot_reply("help", [update_info]);
 
-bot_reply("catto", cat_pictures);
-bot_reply("koko", koala_pictures);
-bot_reply("foxy", fox_pictures);
-bot_reply("ferret", ferret_pictures);
-bot_reply("guwr", gura_pictures);
-bot_reply("panda", panda_pictures);
-bot_reply("lion", lion_pictures);
-bot_reply("awoo", wolf_pictures);
-bot_reply("bnuy", rabbit_pictures);
-bot_reply("meemee", fish_pictures);
+
+user_msg = ["catto", "koko", "foxy", "ferret", "guwr", "panda", "lion", "awoo", "bnuy", "meemee"]
+module_dire = ["cat", "koala", "fox", "ferret", "gura", "panda", "lion", "wolf", "rabbit", "fish"]
+
+for (var i = 0; i < user_msg.length; ++i) {
+    console.log(require('./module_store/' + module_dire[i] + '_module.js'))
+    bot_reply(user_msg[i], (require('./module_store/' + module_dire[i] + '_module.js')))
+}
+
+if (user_msg.length != module_dire.length) {
+    console.log("There is an issue with inputs, oh no")
+}
