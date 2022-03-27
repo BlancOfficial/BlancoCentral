34d4aec92bbffc0cdbb2699e6f5fe4ee413f6381
BlancOfficial <87202988+BlancOfficial@users.noreply.github.com>
2022-03-27 20:08:24 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,11 +8,6 @@ const client = new Discord.Client({ intents: ["GUILDS", "GUILD_MESSAGES"] });
 require('dotenv').config();
 save_list = ["https://cdn.discordapp.com/attachments/806288700736405506/957373290681339984/Error_MSG.png"]
 
-//This needs to be updated with every new version update
-const update_info = "https://cdn.discordapp.com/attachments/806288700736405506/957589966794420224/info_v1.1.1.png"
-const version_Bot = "v1.1.1"
-const update_log = "***UPDATE LOG***\n```Reimplemented Ping Pong... but better\nRepeating commands that had previously been hidden can now be shared in server by pressing the 'Reveal?' button...\n    This feature was soo much harder to figure out than I'd thought it'd be\nOn request, an 'NSFW' command has been added```"
-
 
 client.on('guildCreate', guild => { // Runs when joining a new server
     guild.systemChannel.send(`Thanks for inviting me to the server ^^`)
@@ -80,14 +75,9 @@ client.on('ready', () => { //Checks whether bot is running at logs on startup
 
 client.login(process.env.DISCORD_TOKEN); //Bot accesses discord using Auth Discord Token
 
-//Bot replies that can't be iterated via module_store
-bot_reply("info", ["Currently running " + version_Bot +  " of BlancoBot\n\n" + update_log + "\nhttps://cdn.discordapp.com/attachments/955121751094882336/955525144963596348/Thanks.png\n" + update_info]);
-bot_reply("help", [update_info]);
-
-
-//Setting up user interaction for modules within module_store
-user_msg = ["catto", "koko", "foxy", "ferret", "guwr", "panda", "lion", "awoo", "bnuy", "meemee", "nsfw", "ping", "pong", "a", "info"]
-module_dire = {catto : "cat", koko : "koala", foxy : "fox", ferret : "ferret", guwr : "gura", panda : "panda", lion : "lion", awoo : "wolf", bnuy : "rabbit", meemee : "fish", nsfw : "nsfw", ping : "pong", pong : "pong", a : "a", info : "info"}
+//Setting up user interaction for modules within module_store directory
+user_msg = ["catto", "koko", "foxy", "ferret", "guwr", "panda", "lion", "awoo", "bnuy", "meemee", "nsfw", "ping", "pong", "a", "info", "help"]
+module_dire = {catto : "cat", koko : "koala", foxy : "fox", ferret : "ferret", guwr : "gura", panda : "panda", lion : "lion", awoo : "wolf", bnuy : "rabbit", meemee : "fish", nsfw : "nsfw", ping : "pong", pong : "pong", a : "a", info : "info", help : "help"}
 
 require('events').EventEmitter.defaultMaxListeners = 20; // Current Event Listeners are below this, increased for better slack 
 for (var i = 0; i < user_msg.length; ++i) {
