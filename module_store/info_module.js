00af1b4203371beb87cd97e00f15e9981095f4ca
BlancOfficial <87202988+BlancOfficial@users.noreply.github.com>
2022-03-27 20:22:16 +0100

    1.1.1 hotfix

--- module_store/info_module.js
+++ module_store/info_module.js
@@ -1,5 +1,5 @@
 const thank = "https://cdn.discordapp.com/attachments/955121751094882336/955525144963596348/Thanks.png"
-const update_info = require('./module_store/help_module.js')
+const update_info = "https://cdn.discordapp.com/attachments/806288700736405506/957589966794420224/info_v1.1.1.png"
 const version_Bot = "v1.1.1"
 const update_log = "***UPDATE LOG***\n```Reimplemented Ping Pong... but better\nRepeating commands that had previously been hidden can now be shared in server by pressing the 'Reveal?' button...\n    This feature was soo much harder to figure out than I'd thought it'd be\nOn request, an 'NSFW' command has been added```"
 
