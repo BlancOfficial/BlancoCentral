d46c717a7537e734808b123be6d3b7f134608356
BlancOfficial <87202988+BlancOfficial@users.noreply.github.com>
2022-03-29 23:11:30 +0100

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,9 +1,5 @@
-node -v :: if this doesn't return with a version number, download node.js from https://nodejs.org/en/download/releases/
+node -v
 cd documents
 cd github
 cd blancobot
-git fetch
-git pull
-git fetch
-start cmd /k node .
-start cmd /k node alt_bot.js
+node .
