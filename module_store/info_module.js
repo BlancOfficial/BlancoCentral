7a136e2a4d8317f74933427c25e22fac26ab25ea
BlancOfficial <87202988+BlancOfficial@users.noreply.github.com>
2022-03-29 23:07:40 +0100

    1.1.4

--- module_store/info_module.js
+++ module_store/info_module.js
@@ -1,6 +1,6 @@
 const thank = "https://cdn.discordapp.com/attachments/955121751094882336/955525144963596348/Thanks.png"
 const update_info = "https://cdn.discordapp.com/attachments/885265782966198276/957738591159713822/info_v1.1.2.png"
-const version_Bot = "v1.1.3"
-const update_log = "***UPDATE LOG***\n```Reimplemented Ping Pong... but better\nRepeating commands that had previously been hidden can now be shared in server by pressing the 'Reveal?' button...\n    This feature was soo much harder to figure out than I'd thought it'd be\nOn request, an 'NSFW' command has been added```"
+const version_Bot = "v1.1.4"
+const update_log = "***UPDATE LOG***\n```A Background handler has been added... Essentially, typing 'restart' will bring the bot online if it stops working xD```"
 
 module.exports = ["Currently running " + version_Bot +  " of BlancoBot\n\n" + update_log + "\n" + thank + "\n" + update_info]
---
+++ passive.js
@@ -0,0 +1,21 @@
+const { count } = require('console');
+const Discord = require('discord.js');
+const client = new Discord.Client({ intents: ["GUILDS", "GUILD_MESSAGES"] });
+require('dotenv').config();
+
+client.on('ready', () => { //Checks whether bot is running at logs on startup
+    console.log(`Background Handler is online, ${client.user.tag} active`);
+});
+
+client.login(process.env.DISCORD_TOKEN); //Bot accesses discord using Auth Discord Token
+
+client.on('messageCreate', msg => {
+        if ((msg.content).toLowerCase() === "restart") {
+            require("./bot.js")
+        }
+
+        if ((msg.content).toLowerCase() === "status") {
+            msg.reply("Bot loader: Staus Online")
+            }
+    }
+)
