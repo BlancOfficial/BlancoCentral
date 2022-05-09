227e35f56e0c70e81f1ab8e16bbbbc6309b5e542
BlancOfficial <87202988+BlancOfficial@users.noreply.github.com>
2022-05-09 20:26:42 +0100

    Update info_module.js

--- module_store/info_module.js
+++ module_store/info_module.js
@@ -1,4 +1,4 @@
-const thank = "https://cdn.discordapp.com/attachments/955121751094882336/955525144963596348/Thanks.png"
+const thank = "https://cdn.discordapp.com/attachments/806288700736405506/973295870550360164/Thanks.png"
 const update_info = "https://cdn.discordapp.com/attachments/806288700736405506/970083520854761492/info_v1.1.5.png"
 const version_Bot = "v1.1.5"
 const update_log = "***UPDATE LOG***\n```Added Hug and Pat as commands, planning to make it ping-able but not implemented yet```"
