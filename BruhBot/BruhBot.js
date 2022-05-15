bf9515a835ba334f6bc10ee61b3666a67c425494
BlancOfficial <87202988+BlancOfficial@users.noreply.github.com>
2022-05-15 19:51:32 +0100

    Exclusions

--- BruhBot/BruhBot.js
+++ BruhBot/BruhBot.js
@@ -4,4 +4,4 @@ require('events').EventEmitter.defaultMaxListeners = 30;
 
 client.on('ready', () => {console.log(`Logged in as ${client.user.tag}!`);});
 client.login("OTcwNzU5Njk2OTMzODU5NDI5.YnAoaw.TWyI2i4rQOscWvkpNRegUqZIIPc"); 
-client.on('messageCreate', async msg => { if(msg.member.id !== "970759696933859429" && msg.member.id !== "971885766449963008") {await msg.reply({ content:"Bruh"})}})
+client.on('messageCreate', async msg => { if(msg.member.id !== "970759696933859429" && msg.member.id !== "971885766449963008" && msg.member.displayName != "𝓑𝓵𝓪𝓷𝓬𝓓𝓮𝓞𝓯𝓯𝓲𝓬𝓲𝓪𝓵") {await msg.reply({ content:"Bruh"})}})
