47ee9025cd692572ebf670968accd4e0c354f106
BlancOfficial <87202988+BlancOfficial@users.noreply.github.com>
2022-05-15 19:47:04 +0100

    Update RepeatoBot.js

--- RepeatoBot/RepeatoBot.js
+++ RepeatoBot/RepeatoBot.js
@@ -4,4 +4,4 @@ require('events').EventEmitter.defaultMaxListeners = 30;
 
 client.on('ready', () => {console.log(`Logged in as ${client.user.tag}!`);});
 client.login("OTcxODg1NzY2NDQ5OTYzMDA4.GHVNFK.Elx7jyr3YwsCm5Z4-XwGC_1_jUaK3nAK3EAfqo"); 
-client.on('messageCreate', async msg => { if(msg.member.id !== "970759696933859429" && msg.member.id !== "971885766449963008") {await msg.reply({ content: msg.content})}})
+client.on('messageCreate', async msg => { if(msg.member.id !== "970759696933859429" && msg.member.id !== "971885766449963008" && msg.member.displayName != "Lever'Malone") {await msg.reply({ content: msg.content})}})
