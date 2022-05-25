69fe23f3ee9c3d686abb367fd1f89a210afbcaf1
BlancOfficial <87202988+BlancOfficial@users.noreply.github.com>
2022-05-25 19:29:58 +0100

    Update run.js

--- run.js
+++ run.js
@@ -1,9 +1,3 @@
-const { Client } = require("discord.js")
-const Discord = require('discord.js');
-const client = new Discord.Client({ intents: ["GUILDS", "GUILD_MESSAGES"] });
-
 require("./bot")
 require("./BruhBot/BruhBot")
 require("./RepeatoBot/RepeatoBot")
-
-client.options.shards = 'auto'
