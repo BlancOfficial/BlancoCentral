348d115caebe90383ddaa0c94b262031cc0d20cc
BlancOfficial <87202988+BlancOfficial@users.noreply.github.com>
2022-06-01 15:25:01 +0100

    Update TestoBot.js

--- TestoBot/TestoBot.js
+++ TestoBot/TestoBot.js
@@ -6,16 +6,3 @@ process.on('unhandledRejection', error => {console.log(error)})
 
 client.on('ready', () => {console.log(`Logged in as ${client.user.tag}!`);});
 client.login("OTgxNTQ0NTE2ODQ0NjgzMzM0.GBcAWW.bKoX7YN-8kFa57Vp6HBrA14YvwRBsUFWPdGfXk"); 
-client.on('messageCreate', async msg => { 
-    if(msg.member.id !== "970759696933859429" && msg.member.id !== "971885766449963008" && msg.member.displayName !== "𝓑𝓵𝓪𝓷𝓬𝓓𝓮𝓞𝓯𝓯𝓲𝓬𝓲𝓪𝓵") {
-        await msg.reply({ 
-            embeds: [
-                new MessageEmbed()
-                    .setColor("#0099ff")
-                    .setTitle('Some title')
-                    .setImage('https://i.imgur.com/AfFp7pu.png')
-                    .setTimestamp()
-            ]
-        })
-    }
-})
