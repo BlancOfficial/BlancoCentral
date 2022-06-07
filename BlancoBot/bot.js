acb3c9986a9f4f5b9ba6c672f96016ea2ae7a505
BlancOfficial <87202988+BlancOfficial@users.noreply.github.com>
2022-06-07 18:47:08 +0100

    whitelist patch

--- BlancoBot/bot.js
+++ BlancoBot/bot.js
@@ -3,7 +3,6 @@ const Discord = require('discord.js');
 const client = new Discord.Client({ intents: ["GUILDS", "GUILD_MESSAGES"] });
 const fs = require('fs')
 const { MessageEmbed } = require('discord.js');
-
 save_list = ["https://cdn.discordapp.com/attachments/806288700736405506/957373290681339984/Error_MSG.png"]
 
 process.on('unhandledRejection', error => {})
@@ -26,55 +25,62 @@ require('events').EventEmitter.defaultMaxListeners = 50; // Current Event Listen
 
 fs.readdirSync("./BlancoBot/str_module_store/").forEach(file => {
     client.on('messageCreate', async msg => {
-        if ((msg.content).toLowerCase() === (file.slice(0, - 10)).toLowerCase()) {
-            await msg.reply({
-                content: String(require('./str_module_store/' + file.slice(0, - 10) + '_module.js'))
-                })
+        if (require("./white_list").includes(String(msg.channelId))){
+            if ((msg.content).toLowerCase() === (file.slice(0, - 10)).toLowerCase()) {
+                await msg.reply({
+                    content: String(require('./str_module_store/' + file.slice(0, - 10) + '_module.js'))
+                    })
+                }
             }
         })
     })
 
 fs.readdirSync("./BlancoBot/embed_module_store/").forEach(file => {
     client.on('messageCreate', async msg => {
-        if ((msg.content).toLowerCase() === (file.slice(0, - 10)).toLowerCase()) {
-            await msg.reply({
-                embeds: [require('./embed_module_store/' + file.slice(0, - 10) + '_module.js')]
-                })
+        if (require("./white_list").includes(String(msg.channelId))){
+            if ((msg.content).toLowerCase() === (file.slice(0, - 10)).toLowerCase()) {
+                await msg.reply({
+                    embeds: [require('./embed_module_store/' + file.slice(0, - 10) + '_module.js')]
+                    })
+                }
             }
         })
     })
 
 
 client.on('messageCreate', async msg => {
-    if ((msg.content).toLowerCase().slice(0, 7) === "profile") {
-        if (JSON.parse((JSON.stringify(msg.mentions.users)))[0] === undefined){
-            await msg.reply({
-                embeds: [{
-                    color : (String("#" + Math.floor(Math.random()*16777215).toString(16))),
-                    title: (msg.member.user.tag),
-                    description : ("Current Server Nickname : " + msg.member.displayName),
-                    image : {url : ("https://cdn.discordapp.com/avatars/" + msg.member.user.id + "/" + msg.member.user.avatar + ".png")}
-                    }
-                ]
-            })
-        }
-        
-        else {
-            await msg.reply({
-                embeds: [{
-                    color : (String("#" + Math.floor(Math.random()*16777215).toString(16))),
-                    title: (JSON.parse((JSON.stringify(msg.mentions.users)))[0].tag),
-                    description : ("Current Server Nickname : " + JSON.parse((JSON.stringify(msg.mentions.members)))[0].displayName),
-                    image : {url : ("https://cdn.discordapp.com/avatars/" + JSON.parse((JSON.stringify(msg.mentions.users)))[0].id + "/" + JSON.parse((JSON.stringify(msg.mentions.users)))[0].avatar + ".png")}
-                    }
-                ]
-            })
+    if (require("./white_list").includes(String(msg.channelId))){
+        if ((msg.content).toLowerCase().slice(0, 7) === "profile") {
+            if (JSON.parse((JSON.stringify(msg.mentions.users)))[0] === undefined){
+                await msg.reply({
+                    embeds: [{
+                        color : (String("#" + Math.floor(Math.random()*16777215).toString(16))),
+                        title: (msg.member.user.tag),
+                        description : ("Current Server Nickname : " + msg.member.displayName),
+                        image : {url : ("https://cdn.discordapp.com/avatars/" + msg.member.user.id + "/" + msg.member.user.avatar + ".png")}
+                        }
+                    ]
+                })
+            }
+            
+            else {
+                await msg.reply({
+                    embeds: [{
+                        color : (String("#" + Math.floor(Math.random()*16777215).toString(16))),
+                        title: (JSON.parse((JSON.stringify(msg.mentions.users)))[0].tag),
+                        description : ("Current Server Nickname : " + JSON.parse((JSON.stringify(msg.mentions.members)))[0].displayName),
+                        image : {url : ("https://cdn.discordapp.com/avatars/" + JSON.parse((JSON.stringify(msg.mentions.users)))[0].id + "/" + JSON.parse((JSON.stringify(msg.mentions.users)))[0].avatar + ".png")}
+                        }
+                    ]
+                })
+            }
         }
     }
 })
 
 fs.readdirSync("./BlancoBot/module_store").forEach(file => {
     client.on('messageCreate', async msg => {
+        if (require("./white_list").includes(String(msg.channelId))){
             if ((msg.content).toLowerCase() === (file.slice(0, - 10)).toLowerCase()) {
                 await msg.reply({
                     embeds: [
@@ -106,38 +112,40 @@ fs.readdirSync("./BlancoBot/module_store").forEach(file => {
 
             client.on('interactionCreate', async interaction => { //Function to handle Button Interaction replies
                 if (interaction.isButton()) {
-                    if (interaction.customId === (file.slice(0, - 10)).toLowerCase() + "_repeat") {
-                        await interaction.reply({
-                            embeds: [
-                                new MessageEmbed()
-                                    .setColor(String("#" + Math.floor(Math.random()*16777215).toString(16)))
-                                    .setImage(String(save_list[0] = String(require('./module_store/' + file.slice(0, - 10) + '_module.js')[Math.floor(Math.random() * String(require('./module_store/' + file.slice(0, - 10) + '_module.js').length))])))
-                            ],
-                            ephemeral: true,
-                            components: [
-                                {
-                                    "type": 1,
-                                    "components": [
-                                        {
-                                            "type": 2,
-                                            "label": (file.slice(0, - 10)).toLowerCase() + " Again?",
-                                            "style": 'SUCCESS',
-                                            "custom_id": (file.slice(0, - 10)).toLowerCase() + "_repeat"
-                                        },
-                                        {
-                                            "type": 2,
-                                            "label": "View Original",
-                                            "style": "LINK",
-                                            "url": save_list[0]
+                    if (require("./white_list").includes(String(interaction.channelId))){
+                        if (interaction.customId === (file.slice(0, - 10)).toLowerCase() + "_repeat") {
+                            await interaction.reply({
+                                embeds: [
+                                    new MessageEmbed()
+                                        .setColor(String("#" + Math.floor(Math.random()*16777215).toString(16)))
+                                        .setImage(String(save_list[0] = String(require('./module_store/' + file.slice(0, - 10) + '_module.js')[Math.floor(Math.random() * String(require('./module_store/' + file.slice(0, - 10) + '_module.js').length))])))
+                                ],
+                                ephemeral: true,
+                                components: [
+                                    {
+                                        "type": 1,
+                                        "components": [
+                                            {
+                                                "type": 2,
+                                                "label": (file.slice(0, - 10)).toLowerCase() + " Again?",
+                                                "style": 'SUCCESS',
+                                                "custom_id": (file.slice(0, - 10)).toLowerCase() + "_repeat"
+                                            },
+                                            {
+                                                "type": 2,
+                                                "label": "View Original",
+                                                "style": "LINK",
+                                                "url": save_list[0]
+                                            }
+                                                    ]
                                         }
-                                                ]
-                                    }
-                                ]
-                            })
+                                    ]
+                                })
+                            }
                         }
                     }
                 })
             }
         }
-    )
+    })
 })
---
+++ BlancoBot/white_list.js
@@ -0,0 +1,2 @@
+module.exports = ["971130499638636634", "979100024464113664", "955251411300536442", "890039940782645282", "929502495070359572"]
+                //Channel Main Server     Test Main Server      Foxy Server         General Krillin        Movie Krillin
