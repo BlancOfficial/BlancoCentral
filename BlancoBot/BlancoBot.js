637084106ea741d6ebb55774099094f66c5bc969
BlancOfficial <87202988+BlancOfficial@users.noreply.github.com>
2022-06-22 20:02:47 +0100

    blacklist update

--- BlancoBot/BlancoBot.js
+++ BlancoBot/BlancoBot.js
@@ -2,7 +2,7 @@
 const { verify } = require('crypto');
 const Discord = require('discord.js');
 const client = new Discord.Client({ intents: ["GUILDS", "GUILD_MESSAGES"] });
-const fs = require('fs')
+const fs = require('fs');
 save_list = ["https://cdn.discordapp.com/attachments/974423774877347891/987468861752352818/BlancoError.png"]
 const thanks = "https://cdn.discordapp.com/attachments/806288700736405506/973295870550360164/Thanks.png"
 const trying = "https://cdn.discordapp.com/attachments/974423774877347891/984579953830019072/Keep_Trying.png"
@@ -10,15 +10,20 @@ const oh = "https://cdn.discordapp.com/attachments/974423774877347891/9888305723
 const whitelist = [require("./modules/admin_module/white_list.json")] //User and Channel whitelist
 const blacklist = [require("./modules/admin_module/black_list.json")] //User and Channel blacklist
 var data = []
-process.on('unhandledRejection', error => {console.log(error)}) //>.> if it works, it works
-
 
 function verifyMSG(msg){
     if (whitelist[0].servers.includes(String(msg.guildId)) || whitelist[0].channels.includes(String(msg.channelId))){
-        if (blacklist[0].servers.includes(String(msg.guildId)) || blacklist[0].channels.includes(String(msg.channelId))){return false}
+        if (blacklist[0].servers.includes(String(msg.guildId)) || blacklist[0].channels.includes(String(msg.channelId)) || blacklist[0].users.includes(String(msg.member.user.id))){}
         else {return true}}
     return false}
 
+client.on('ready', () => {console.log(`Logged in as ${client.user.tag}!`)})
+
+client.login("OTU1MTE5NTUwMDU4MzQ4NTg1.YjdCZQ.iZlAabxKBwCgK8SPe7N1sKOyTbE"); //Bot accesses discord using Auth Discord Token
+
+require('events').EventEmitter.defaultMaxListeners = 80; // Current Event Listeners are below this, increased for better slack 
+
+process.on('unhandledRejection', error => {console.log(error)}) //>.> if it works, it works
 
 client.on('guildCreate', guild => { // Runs when joining a new server
     guild.systemChannel.send(`Thanks for inviting me to the server ^^`)
@@ -26,14 +31,6 @@ client.on('guildCreate', guild => { // Runs when joining a new server
     guild.systemChannel.send(`help`)
     guild.systemChannel.send(`*pout* Seems my creator hasn't activated me just yet, DM him at 𝓑𝓵𝓪𝓷𝓬𝓓𝓮𝓞𝓯𝓯𝓲𝓬𝓲𝓪𝓵#2485`)
     })
-  
-client.on('ready', () => { //Checks whether bot is running at logs on startup
-    console.log(`Logged in as ${client.user.tag}!`);
-    })
-
-client.login("OTU1MTE5NTUwMDU4MzQ4NTg1.YjdCZQ.iZlAabxKBwCgK8SPe7N1sKOyTbE"); //Bot accesses discord using Auth Discord Token
-
-require('events').EventEmitter.defaultMaxListeners = 80; // Current Event Listeners are below this, increased for better slack 
 
 
 fs.readdirSync("./BlancoBot/modules/str_module_store/").forEach(file => {
@@ -148,90 +145,97 @@ client.on('messageCreate', async msg => {
 client.on('messageCreate', async msg => { //admin
     if (msg.content.toLowerCase().slice(0, 6) === "admin."){
         if (whitelist[0].users.includes(String(msg.author.id)) ===  true){
-            if ((msg.content).toLowerCase() === "admin.setup"){
-                    if (whitelist[0].channels.includes(msg.channelId) === false){
-                        whitelist[0].channels.push(msg.channelId)
-                        fs.writeFile("BlancoBot/modules/admin_module/white_list.json", JSON.stringify(whitelist[0]), err => {})
+            if ((msg.content).toLowerCase().slice(0, 11) === "admin.setup"){
+                if ((msg.content).toLowerCase() === "admin.setup"){
+                        if (whitelist[0].channels.includes(msg.channelId) === false){
+                            whitelist[0].channels.push(msg.channelId)
+                            fs.writeFile("BlancoBot/modules/admin_module/white_list.json", JSON.stringify(whitelist[0]), err => {})
+                            await msg.reply({
+                                embeds: [{
+                                    color : (String("#" + Math.floor(Math.random()*16777215).toString(16))),
+                                    title : ("Channel has been Activated"),
+                                    image : {url : thanks},
+                                }]})}
+                        else {
+                            await msg.reply({
+                                embeds : [
+                                    {
+                                        color : (String("#" + Math.floor(Math.random()*16777215).toString(16))),
+                                        title: "This Channel Has Already Been Activated",
+                                        image: {url: trying}
+                                    }]})}}
+
+                else if (msg.content.toLowerCase() === "admin.setup.priority"){
+                        if (whitelist[0].servers.includes(msg.guildId) === false){
+                            whitelist[0].servers.push(msg.guildId)
+                            fs.writeFile("BlancoBot/modules/admin_module/white_list.json", JSON.stringify(whitelist[0]), err => {})
+                            await msg.reply({
+                                embeds: [{
+                                    color : (String("#" + Math.floor(Math.random()*16777215).toString(16))),
+                                    title : ("Server has been Activated"),
+                                    image : {url : thanks},
+                                }]})}
+                        else {
+                            await msg.reply({
+                                embeds : [
+                                    {
+                                        color : (String("#" + Math.floor(Math.random()*16777215).toString(16))),
+                                        title: "This Server Has Already Been Activated",
+                                        image: {url: trying}
+                                    }]})}}}
+
+            else if (msg.content.toLowerCase().slice(0, 13) === "admin.control"){
+
+                if (msg.content.toLowerCase().slice(0, 20) == "admin.control.member"){
+                    if (JSON.parse((JSON.stringify(msg.mentions.users)))[0].id === undefined){msg.reply("undef Error")}
+                    else{
+                        if (blacklist[0].users.includes(JSON.parse((JSON.stringify(msg.mentions.users)))[0].id) === false){
+                            blacklist[0].users.push(JSON.parse((JSON.stringify(msg.mentions.users)))[0].id)
+                            fs.writeFile("BlancoBot/modules/admin_module/black_list.json", JSON.stringify(blacklist[0]), err => {}),
+                            await msg.reply({
+                                embeds: [{
+                                    color : (String("#" + Math.floor(Math.random()*16777215).toString(16))),
+                                    title : JSON.parse((JSON.stringify(msg.mentions.members)))[0].displayName + " has been blocked by BlancoBot",
+                                    image : {url : oh}
+                                }]})}}}
+
+                else if ((msg.content).toLowerCase() === "admin.control"){
+                    if (blacklist[0].channels.includes(msg.channelId) === false){
+                        blacklist[0].channels.push(msg.channelId)
+                        fs.writeFile("BlancoBot/modules/admin_module/black_list.json", JSON.stringify(blacklist[0]), err => {})
                         await msg.reply({
                             embeds: [{
                                 color : (String("#" + Math.floor(Math.random()*16777215).toString(16))),
-                                title : ("Channel has been Activated"),
-                                image : {url : thanks},
+                                title : ("Channel has been Blocked by BlancoBot"),
+                                image : {url : oh},
                             }]})}
                     else {
                         await msg.reply({
                             embeds : [
                                 {
                                     color : (String("#" + Math.floor(Math.random()*16777215).toString(16))),
-                                    title: "This Channel Has Already Been Activated",
-                                    image: {url: trying}
+                                    title: "This Channel Has Already Been Blocked by BlancoBot",
+                                    image: {url: oh}
                                 }]})}}
-            else if (msg.content.toLowerCase() === "admin.setup.priority"){
-                    if (whitelist[0].servers.includes(msg.guildId) === false){
-                        whitelist[0].servers.push(msg.guildId)
-                        fs.writeFile("BlancoBot/modules/admin_module/white_list.json", JSON.stringify(whitelist[0]), err => {})
+
+                else if (msg.content.toLowerCase() === "admin.control.priority"){
+                    if (blacklist[0].servers.includes(msg.guildId) === false){
+                        blacklist[0].servers.push(msg.guildId)
+                        fs.writeFile("BlancoBot/modules/admin_module/black_list.json", JSON.stringify(blacklist[0]), err => {})
                         await msg.reply({
                             embeds: [{
                                 color : (String("#" + Math.floor(Math.random()*16777215).toString(16))),
-                                title : ("Server has been Activated"),
-                                image : {url : thanks},
-                            }]})}
-                    else {
-                        await msg.reply({
-                            embeds : [
-                                {
-                                    color : (String("#" + Math.floor(Math.random()*16777215).toString(16))),
-                                    title: "This Server Has Already Been Activated",
-                                    image: {url: trying}
-                                }]})}}
-            else if (msg.content.toLowerCase().slice(0, 20) == "admin.control.member"){
-                if (JSON.parse((JSON.stringify(msg.mentions.users)))[0].id === undefined){msg.reply("undef Error")}
-                else{
-                    if (blacklist[0].users.includes(JSON.parse((JSON.stringify(msg.mentions.users)))[0].id) === false){
-                        blacklist[0].users.push(JSON.parse((JSON.stringify(msg.mentions.users)))[0].id)
-                        fs.writeFile("BlancoBot/modules/admin_module/black_list.json", JSON.stringify(blacklist[0]), err => {}),
-                        await msg.reply({
-                            embeds: [{
-                                color : (String("#" + Math.floor(Math.random()*16777215).toString(16))),
-                                title : JSON.parse((JSON.stringify(msg.mentions.members)))[0].displayName + " has been blocked by BlancoBot",
+                                title : ("Server has been Blocked by BlancoBot"),
                                 image : {url : oh}
-                            }]})}}}
-            else if ((msg.content).toLowerCase() === "admin.control"){
-                if (blacklist[0].channels.includes(msg.channelId) === false){
-                    blacklist[0].channels.push(msg.channelId)
-                    fs.writeFile("BlancoBot/modules/admin_module/black_list.json", JSON.stringify(blacklist[0]), err => {})
-                    await msg.reply({
-                        embeds: [{
-                            color : (String("#" + Math.floor(Math.random()*16777215).toString(16))),
-                            title : ("Channel has been Blocked by BlancoBot"),
-                            image : {url : oh},
-                        }]})}
-                else {
-                    await msg.reply({
-                        embeds : [
-                            {
-                                color : (String("#" + Math.floor(Math.random()*16777215).toString(16))),
-                                title: "This Channel Has Already Been Blocked by BlancoBot",
-                                image: {url: oh}
-                            }]})}}
-            else if (msg.content.toLowerCase() === "admin.control.priority"){
-                if (blacklist[0].servers.includes(msg.guildId) === false){
-                    blacklist[0].servers.push(msg.guildId)
-                    fs.writeFile("BlancoBot/modules/admin_module/black_list.json", JSON.stringify(blacklist[0]), err => {})
-                    await msg.reply({
-                        embeds: [{
-                            color : (String("#" + Math.floor(Math.random()*16777215).toString(16))),
-                            title : ("Server has been Blocked by BlancoBot"),
-                            image : {url : oh}
-                        }]})}
-                else {
-                    await msg.reply({
-                        embeds : [
-                            {
-                                color : (String("#" + Math.floor(Math.random()*16777215).toString(16))),
-                                title: "This Server Has Already Been Blocked By BlancoBot",
-                                image: {url: oh}
-                            }]})}}}
+                            }]})}
+                    else {
+                        await msg.reply({
+                            embeds : [
+                                {
+                                    color : (String("#" + Math.floor(Math.random()*16777215).toString(16))),
+                                    title: "This Server Has Already Been Blocked By BlancoBot",
+                                    image: {url: oh}
+                                }]})}}}}
     else {
         await msg.reply({
             embeds : [
