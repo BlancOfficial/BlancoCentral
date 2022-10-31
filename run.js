b3fd59c75113ed8cbe28095d97989b49812908c3
BlancOfficial <87202988+BlancOfficial@users.noreply.github.com>
2022-10-31 11:09:55 +0000

    Update run.js

--- run.js
+++ run.js
@@ -1,19 +1,11 @@
-if (require("./auth.json").BlancoBot == "")
-    {
-        console.log("\nERROR: Enter BlancoBot Tokens inside './auth.json'\n")
-    }
-else { require("./BlancoBot/BlancoBot") }
+if (require("./auth.json").BlancoBot == "") { console.log("\nERROR: Enter BlancoBot Tokens inside './auth.json'\n") }
+else { console.log("Token has been read... Authenticating"); require("./BlancoBot/BlancoBot") };
 
-if (require("./auth.json").Bruh == "")
-    {
-        console.log("\nERROR: Enter BruhBot Tokens inside './auth.json'\n")
-    }
-else { require("./BruhBot/BruhBot") }
+if (require("./auth.json").Bruh == "") { console.log("\nERROR: Enter BruhBot Tokens inside './auth.json'\n") }
+else { console.log("Token has been read... Authenticating"); require("./BruhBot/BruhBot") };
 
-if (require("./auth.json").Repeato == "")
-    {
-        console.log("\nERROR: Enter RepeatoBot Tokens inside './auth.json'\n")
-    }
-else { require("./RepeatoBot/RepeatoBot") }
+if (require("./auth.json").Repeato == "") { console.log("\nERROR: Enter RepeatoBot Tokens inside './auth.json'\n") }
+else { console.log("Token has been read... Authenticating"); require("./RepeatoBot/RepeatoBot") };
 
-//require("./TestoBot/TestoBot")
+//if (require("./auth.json").Testo == "") { console.log("\nERROR: Enter TestoBot Tokens inside './auth.json'\n") }
+//else { console.log("Token has been read... Authenticating"); require("./TestoBot/TestoBot") };
