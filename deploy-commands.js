eb681c9ee02756880566170beda9fdfa659943ab
BlancOfficial <87202988+BlancOfficial@users.noreply.github.com>
2023-01-06 14:06:56 +0000

    Update deploy-commands.js

--- deploy-commands.js
+++ deploy-commands.js
@@ -5,7 +5,7 @@ const fs = require('node:fs');
 
 const commands = [];
 // Grab all the command files from the commands directory you created earlier
-const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+const commandFiles = fs.readdirSync('./BlancoBot/commands').filter(file => file.endsWith('.js'));
 
 
 if (app_ID == "") { console.log("Error : No Application ID has been entered into './auth.json'")}
@@ -13,14 +13,13 @@ if (app_ID == "") { console.log("Error : No Application ID has been entered into
 
 // Grab the SlashCommandBuilder#toJSON() output of each command's data for deployment
 for (const file of commandFiles) {
-	const command = require(`./commands/${file}`);
+	const command = require(`./BlancoBot/commands/${file}`);
 	commands.push(command.data.toJSON());
 }
 
-// Construct and prepare an instance of the REST module
+// Construct + Prepares an instance of the REST module
 const rest = new REST({ version: '10' }).setToken(token);
 
-// and deploy your commands!
 (async () => {
 	try {
 		console.log(`Started refreshing ${commands.length} application (/) commands.`);
@@ -33,7 +32,6 @@ const rest = new REST({ version: '10' }).setToken(token);
 
 		console.log(`Successfully reloaded ${data.length} application (/) commands.`);
 	} catch (error) {
-		// And of course, make sure you catch and log any errors!
 		console.error(error);
 	}
 })();
