238f19d8773d6cbebfd9810cb9bfa3052a1414b0
BlancOfficial <87202988+BlancOfficial@users.noreply.github.com>
2023-01-06 15:44:03 +0000

    auth update

--- run.js
+++ run.js
@@ -1,17 +1,8 @@
-fail_chck=0; chck=0
-
 function startup(_JSON, BotName){
-    chck=chck+1
-    if (_JSON == "") {fail_chck=fail_chck+1; console.log("\nERROR: Enter " + BotName + " Tokens inside './auth.json'\n") }
+    if (_JSON == "") {console.log("\nERROR: Enter " + BotName + " Tokens inside './auth.json'\n") }
     else { console.log(BotName + "'s Token has been read... Authenticating"); require("./" + BotName + "/" + BotName) }
 }
 
 startup(require("./auth.json").BlancoBot, "BlancoBot")
 startup(require("./auth.json").Bruh, "BruhBot")
 startup(require("./auth.json").Repeato, "RepeatoBot")
-//startup(require("./auth.json").Testo, "TestoBot")
-
-if(fail_chck=chck)
-    {
-        console.log("ERROR: No Tokens have been entered, Program failed to execute")
-    }
