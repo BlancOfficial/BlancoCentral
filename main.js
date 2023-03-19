969a9036ae5682edb0851f4b44e3b516b7395be5
BlancOfficial <87202988+BlancOfficial@users.noreply.github.com>
2023-03-19 14:26:05 +0000

    Package-Baggage strip

--- main.js
+++ main.js
@@ -18,41 +18,26 @@ client.once(Events.ClientReady, c =>
 
 client.login(require("./auth.json").BlancoBot); //Bot accesses discord using Auth Discord Token
 
-client.on('guildCreate', guild => { // Runs when joining a new server
-    guild.systemChannel.send(`Thanks for inviting me to the server ^^`)
-    });
+// Runs when bot joins a new server
+client.on('guildCreate', guild => { guild.systemChannel.send(`Thanks for inviting me to the server ^^`) });
 
-client.on('guildMemberAdd', new_member => { //DM new user
-    new_member.send(`Welcome New Friend!`)
-    new_member.send("https://cdn.discordapp.com/attachments/974423774877347891/993944924720480398/Welcome.png")
-});
-
-
+//Checking all .js files in the command path for all components
 for (const file of commandFiles) {
 	const filePath = path.join(commandsPath, file);
 	const command = require(filePath);
 
-	if ('data' in command && 'execute' in command) {
-		client.commands.set(command.data.name, command);
-	} else {
-		console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
-	}
+	if ('data' in command && 'execute' in command) { client.commands.set(command.data.name, command); } 
+	else { console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`); }
 }
 
+//Command Event listeners
 client.on(Events.InteractionCreate, async interaction => {
 	if (!interaction.isChatInputCommand()) return;
-
 	const command = interaction.client.commands.get(interaction.commandName);
 
-	if (!command) {
-		console.error(`No command matching ${interaction.commandName} was found.`);
-		return;
-	}
+	if (!command) { console.error(`No command matching ${interaction.commandName} was found.`); return; }
 
-	try {
-		await command.execute(interaction);
-	} catch (error) {
-		console.error(error);
-		await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+	try { await command.execute(interaction); } 
+	catch (error) { console.error(error); await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true }); }
 	}
-});
+);
